1aa80af30899443c8ecb4870a346667f176769b1
llycherrie <101507294+llycherrie@users.noreply.github.com>
2022-10-12 14:38:06 +1000

    Create adoption.php

---
+++ src/adoption.php
@@ -0,0 +1,43 @@
+<?php
+include_once 'header.php';
+?>
+         <!--First section-->
+                <div style="background-image: url('https://dogsbestlife.com/wp-content/uploads/2021/02/cats-vs.-dogs-group-scaled.jpeg'); 
+                            background-size: cover; height:500px; padding-top:80px;margin:50px; text-align: center;">
+
+<p style="font-size:30px; color: black;"><em>They can be your best friend! Give them a home!</em></p>
+
+                </div>
+                        <div class="row">
+  <div class="column">
+<p>
+<a href=dog.html">
+<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/cruze3-320x320.jpg"  width="200" height="200">
+</a>
+<h1 style="font-size:30px; color: Blue;">Dogs</h1>
+</p>
+</div>
+
+
+ <div class="column">
+
+<p>
+<a href=cat.html">
+<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/dannie-320x320.jpg"  width="200" height="200">
+</a>
+<h1 style="font-size:30px; color: Blue;">Cats</h1>
+</p>
+ </div>
+
+<div class="column">
+        <p>
+<a href=bird.html">
+<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/DSC05286-320x320.jpg" width="200" height="200">
+</a>
+<h1 style="font-size:30px; color: Blue;">Birds</h1>
+</p>
+
+
+                </div>
+</body>
+</html>
