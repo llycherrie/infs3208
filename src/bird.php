aab9f41caa1f95de307bd559abfa30b089d1b470
llycherrie <101507294+llycherrie@users.noreply.github.com>
2022-10-12 20:24:15 +1000

    Create bird.php

---
+++ src/bird.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'header.php';
+?>
+         <!--First section-->
+                <div style="background-image: url('https://www.thesprucepets.com/thmb/XIvUm4yKZU46UryJ-Kgup1fKcSw=/3600x2400/filters:fill(auto,1)/popular-small-bird-species-390926-hero-d3d0af7bb6ed4947b0c3c5afb4784456.jpg'); 
+                            background-size: cover; height:500px; padding-top:80px;margin:50px; text-align: center;">
+
+</div>
+
+<div style="height:600px; margin:100px;text-align: center;">
+
+<p style="font-size:100px; color: black;"><em>Adopt a bird!</em></p>
+
+<h1 style="font-size:30px; color: Blue;">Sorry, No birds are available for adoption at this time, please come back again later.</h1>
