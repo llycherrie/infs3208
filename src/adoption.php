406dcdff104f5a1e569bdbf7c3e7e7cc0437a148
llycherrie <101507294+llycherrie@users.noreply.github.com>
2022-10-12 20:35:40 +1000

    Update adoption.php

--- src/adoption.php
+++ src/adoption.php
@@ -5,14 +5,15 @@ include_once 'header.php';
                 <div style="background-image: url('https://dogsbestlife.com/wp-content/uploads/2021/02/cats-vs.-dogs-group-scaled.jpeg'); 
                             background-size: cover; height:500px; padding-top:80px;margin:50px; text-align: center;">
 
-<p style="font-size:30px; color: black;"><em>They can be your best friend! Give them a home!</em></p>
+<p style="font-size:30px; color: black;"><em>              They can be your best friend! Give them a home!</em></p>
 
                 </div>
+
                         <div class="row">
   <div class="column">
 <p>
-<a href=dog.html">
-<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/cruze3-320x320.jpg"  width="200" height="200">
+<a href=./adoption/dog.php>
+<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/cruze3-320x320.jpg"  style="width:100%">
 </a>
 <h1 style="font-size:30px; color: Blue;">Dogs</h1>
 </p>
@@ -22,8 +23,9 @@ include_once 'header.php';
  <div class="column">
 
 <p>
-<a href=cat.html">
-<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/dannie-320x320.jpg"  width="200" height="200">
+<a href=cat.php>
+<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/dannie-320x320.jpg"  style="width:100%">
+
 </a>
 <h1 style="font-size:30px; color: Blue;">Cats</h1>
 </p>
@@ -31,13 +33,39 @@ include_once 'header.php';
 
 <div class="column">
         <p>
-<a href=bird.html">
-<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/DSC05286-320x320.jpg" width="200" height="200">
+<a href=bird.php>
+<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/DSC05286-320x320.jpg" style="width:100%">
 </a>
 <h1 style="font-size:30px; color: Blue;">Birds</h1>
 </p>
 
+</div>
 
-                </div>
-</body>
-</html>
+<div class="column">
+        <p>
+<a href=./adoption/farmAnimal.php>
+<img border="20" alt="W3Schools" src="https://www.awlqld.com.au/wp-content/uploads/2020/08/69297281_10156792908867547_5539275581427810304_o-320x320.jpg" style="width:100%">
+</a>
+<h1 style="font-size:30px; color: Blue;">Farm Animals</h1>
+</p>
+
+</div>
+
+
+<div class="column">
+        <p>
+<a href=./adoption/reptile.php>
+<img border="20" alt="W3Schools" src="https://lirp.cdn-website.com/a6496801/dms3rep/multi/opt/Reptile%2B1-640w.jpg" style="width:100%">
+</a>
+<h1 style="font-size:30px; color: Blue;">Reptiles</h1>
+</p>
+
+</div>
+
+</div>
+<div style="height:600px; margin:100px;text-align: center;">
+
+                <h1>Join Us!</h1>
+         <h2>Our big-picture hope is for every pet in Australia to be safe, respected and loved - whether the pet is owned, surrendered to a shelter, unclaimed at the pound, seized by authorities, or trapped by rangers!</h2>
+         <h3>@Brisbane, QLD | Since 2022</h3>
+</div>
