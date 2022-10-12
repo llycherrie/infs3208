21c4f920c0b065888915c4807375d71786c852d6
llycherrie <101507294+llycherrie@users.noreply.github.com>
2022-10-12 16:04:22 +1000

    Update header.php

--- src/header.php
+++ src/header.php
@@ -25,11 +25,11 @@ session_start();
                 <li><a href="#">Donation</a></li>
 
                 <?php
-                        if (isset($_SESSION["Username"])){
+                        if (isset($_SESSION["userName"])){
                                 echo "<li><a href='profile.php'>Profile</a></li>";
-                                echo "<li><a href='logout.php'>Logout</a></li>";
+                                echo "<li><a href='includes/logout.inc.php'>Logout</a></li>";
 
-                        }
+                        } 
                         else {
                                 echo " <li><a href='register.php'>Register</a></li>";
                                 echo "<li><a href='login.php'>Login</a></li>";
