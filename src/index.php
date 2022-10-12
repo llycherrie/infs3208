861bad03696b77afff11f82a83812369bb772c91
llycherrie <101507294+llycherrie@users.noreply.github.com>
2022-10-12 14:30:45 +1000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,30 @@
-<?php 
-echo "I am a PHP page running on four containers: NginX + PHP-PFM + MariaDB + Redis!";
-phpinfo();
-?>
+<?php
+ include_once 'header.php';
+ ?>
+
+                <!--First section-->
+                <div style="background-image: url('https://images.unsplash.com/photo-1548266652-99cf27701ced?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80'); 
+                            background-size: cover; height:480px; padding-top:80px; text-align: center;">
+
+
+        <img src="https://e7.pngegg.com/pngimages/974/35/png-clipart-dog-cat-animal-rescue-group-animal-shelter-dog-blue-animals.png" style="height:150px; border-radius: 50%; border: 10px solid #FEDE00;" alt="This is a small profile image of the logo">
+
+        <h1 style="font-size:100px; color:white; margin:10px;">Brisbane Animal Shelter</h1>
+        <p style="font-size:30px; color: white;"><em>Adopt! Don't Shop!</em></p>
+
+        <p style="font-size: 20px; color:#1F9AFE;">
+
+                </div>
+
+
+        <!--Second section-->
+        <img src="https://www.supermarketnews.com/sites/supermarketnews.com/files/pet-care-coronavirus-happy-dog-and-cat_0.png" style="height:600px; margin:100px; float: left;" alt="A pic of dog and cat">
+
+        <div style="height:600px; margin:100px;text-align: center;">
+                <h1>Join Us!</h1>
+         <h2>Our big-picture hope is for every pet in Australia to be safe, respected and loved - whether the pet is owned, surrendered to a shelter, unclaimed at the pound, seized by authorities, or trapped by rangers!</h2>
+         <h3>@Brisbane, QLD | Since 2022</h3>
+        </div>
+
+        </body>
+</html>
