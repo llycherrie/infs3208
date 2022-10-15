ebae0a4ce93f5921ae9529130455990f85554c93
llycherrie <101507294+llycherrie@users.noreply.github.com>
2022-10-15 17:36:15 +1000

    Create cat.php

---
+++ src/cat.php
@@ -0,0 +1,59 @@
+<?php
+include_once 'header.php';
+?>
+
+  <div style="background-image: url('https://applaws.com/us/wp-content/uploads/sites/4/2014/12/Did_you_know-1.jpg'); 
+                            background-size: cover; height:500px; padding-top:80px;margin:50px; text-align: center;">
+
+</div>
+
+<div style="text-align: center;">
+
+<p style="font-size:100px; color: black;"><em>Adopt a cat!</em></p>
+
+</div>
+<div class="row">
+  <div class="column">
+<a href="cat1.php">
+  <figure>
+    <img src="https://www.awlqld.com.au/wp-content/uploads/2022/10/Charity-451695-200x191.jpg" alt="demo image" />
+    <figcaption>CHARITY<br><br>Breed: Domestic Medium Hair<br> Gender: Female<br> Age: 2 Years<br>Location: Gold Coast, Rehoming Centre </figcaption>
+  </figure>
+</a>
+</div>
+
+ <div class="column">
+<a href="cat2.php">
+  <figure>
+    <img src="https://www.awlqld.com.au/wp-content/uploads/2022/10/Jigsaw-442726-200x200.jpg" alt="demo image" />
+    <figcaption>JIGSAW<br><br>Breed: Domestic Short Hair<br> Gender: Male<br> Age: 7 Years<br>Location: Gold Coast, Rehoming Centre </figcaption>
+  </figure>
+</a>
+</div>
+
+ <div class="column">
+<a href="cat3.php">
+  <figure>
+    <img src="https://www.awlqld.com.au/wp-content/uploads/2022/10/Caramello-451634-200x200.jpg" alt="demo image" />
+    <figcaption>CARAMELLO<br><br>Breed: Domestic Short Hair<br> Gender: Male<br> Age: 4 Years<br>Location: Gold Coast, Rehoming Centre </figcaption>
+  </figure>
+</a>
+</div>
+
+ <div class="column">
+<a href="cat4.php">
+  <figure>
+    <img src="https://www.awlqld.com.au/wp-content/uploads/2022/10/Sergeant-Meow-Meow-446739-200x200.jpg" alt="demo image" />
+    <figcaption>SERGANT<br><br>Breed: Domestic Medium Hair<br> Gender: Male<br> Age: 9 Years<br>Location: Ipswich, Rehoming Centre </figcaption>
+  </figure>
+</a>
+
+</div>
+
+<div style="height:600px; margin:100px;text-align: center;">
+
+                <h1>PET ADOPTION PROCESS</h1>
+         <h2>If you are interested in adopting our animals here, Please visit our centres!
+ </h2>
+         <h3>@Brisbane, QLD | Since 2022</h3>
+</div>
