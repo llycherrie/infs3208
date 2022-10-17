871038be9d749e0cc95b42e463eae31724da8302
llycherrie <101507294+llycherrie@users.noreply.github.com>
2022-10-17 14:00:59 +1000

    Update register.php

--- src/register.php
+++ src/register.php
@@ -9,7 +9,7 @@
         <img src="https://media.istockphoto.com/photos/happy-mixed-breed-dog-posing-with-a-kitten-on-his-head-picture-id1210341751?k=20&m=1210341751&s=612x612&w=0&h=v0Dw0214h_1zUG1bZTXx8IG0QwfOwi3Iqd52QNii6ZI=" style="height:600px; margin:30px; float: left;" alt="A pic of dog and cat">
 
         <div style=" height:600px; margin:30px;">
-                <form action="includes/signup.inc.php" method="post">
+                <form action="include/signup.inc.php" method="post">
                         <input type="text" name="name" placeholder="Full name..."><br>
                          <input type="text" name="email" placeholder="email..."><br>
                          <input type="text" name="username" placeholder="Username..."><br>
